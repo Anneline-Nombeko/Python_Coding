b0bec847392f661b952fe6c9e8e7f3a90485c8c2
Anneline-Nombeko <a.nombeko@outlook.com>
2023-07-27 14:22:35 +0200

    strings in python

---
+++ 0_strings.py
@@ -0,0 +1,12 @@
+message = "meet me tonight"
+print(message)
+message2 = 'The clock strikes at midnight.'
+print(message2)
+message3 = "i\'m busy right now"
+print(message3)
+
+message5 = """one of my favourites lines from the God father is:
+ "i'm going to make him an offer he cant refuse."
+  do you know who said this?"""
+print(message5)
+
