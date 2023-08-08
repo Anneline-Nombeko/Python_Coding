6228b07b2bc9f98616477f61f82b8437e0c01cf5
Anneline-Nombeko <a.nombeko@outlook.com>
2023-08-08 13:45:27 +0200

    classes.py

---
+++ 0_classes.py
@@ -0,0 +1,19 @@
+class User:
+ pass
+
+user1 = User()
+user1.first_name = "anne"
+user1.last_name = "Nombeko"
+user1.age = 34
+user1.favourite_book = "2001: A space odyssey"
+
+user2 = user()
+user2.age = 21
+user2.favourite_book = "iqhina lomtshato"
+
+
+
+print(user1.first_name)
+print(user1.last_name)
+
+
